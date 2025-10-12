bb0518903b474ff80956905d096be5def0aa28df
applzz <hws0602@naver.com>
2025-10-13 00:45:16 +0900

    fix: 수정 사항 적용

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,14 +2,12 @@ import api from "@/lib/api";
 
 const axios = api;
 
-export const postLogin =async ({email,password}) =>{
+export const postLogin = async ({email,password}) =>{
     const result = await axios.post('/auth/login', {email,password})
-    console.log('login result: ', result)
     return result.data;
 }
 
-export const postSignUp =async ({email,password}) =>{
+export const postSignUp = async ({email,password}) =>{
     const result = await axios.post('/auth/register', {email,password})
-    console.log('signup result: ', result)
     return result.data;
 }
